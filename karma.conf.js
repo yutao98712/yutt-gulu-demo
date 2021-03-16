11a146a3aea62c27a50e9d4af40ab32c7c4d7fd3
yutao98712 <1426290941@qq.com>
2021-03-16 15:46:57 +0800

    travis_ci编译失败，使用无头浏览器

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     concurrency: Infinity
   })
